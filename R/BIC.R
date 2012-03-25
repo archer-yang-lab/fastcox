855eb2ce76ca07e2517fdfca2bc116e8bcf2583a
Yi Yang <emeryyi@gmail.com>
2012-03-25 14:02:48 -0500

    1. fixed AIC and BIC

--- R/BIC.R
+++ R/BIC.R
@@ -4,7 +4,7 @@ BIC <- function(fit, x, y, d){
 	beta = fit$beta
     loglik = OBJ(y=y,x=x,d=d,beta=beta)
 	df = fit$df
- 	BIC = 2 * loglik + log(nobs) * df
+ 	BIC = 2 * nobs * loglik + log(nobs) * df
 	cmin = min(BIC)
 	idmin = BIC <= cmin
 	lambda.min = max(fit$lambda[idmin])
